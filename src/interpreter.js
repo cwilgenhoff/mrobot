668c58079cec3c6f87ea04707afacd9d17ceaaea
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2017-02-28 22:56:44 +0000

    Added file interpreter

---
+++ src/interpreter.js
@@ -0,0 +1,86 @@
+import { createInterface } from 'readline';
+import { createReadStream } from 'fs';
+
+import Grid from './grid';
+import Robot from './robot';
+import { configureInstructions } from './instructions/index';
+
+class Interpreter {
+  constructor(filePath) {
+    this.instructions = configureInstructions();
+    this.lineReader = createInterface({
+      input: createReadStream(filePath),
+    });
+    this.PATTERNS = {
+      SET_GRID_MAX_POINT: /^[0-9]\s[0-9]$/,
+      SET_ROBOT_POSITION_AND_ORIENTATION: /^[0-9]\s[0-9]\s[NESW]$/,
+      INSTRUCTIONS: /^[A-Z]+$/,
+    };
+    this.robots = [];
+  }
+
+  arrayFrom(line) {
+    return line.trim().split('').filter(i => i !== ' ');
+  }
+
+  parseGridMaxPoint(line) {
+    const input = this.arrayFrom(line);
+    return {
+      x: Number.parseInt(input[0], 10),
+      y: Number.parseInt(input[1], 10)
+    };
+  }
+
+  parseRobotPositionAndOrientation(line) {
+    const input = this.arrayFrom(line);
+    return {
+      x: Number.parseInt(input[0], 10),
+      y: Number.parseInt(input[1], 10),
+      orientation: input[2]
+    };
+  }
+
+  parseInstructions(line) {
+    const input = this.arrayFrom(line);
+    return input.map(char => this.instructions[char])
+  }
+
+  getLastAddedRobot() {
+    return this.robots[this.robots.length - 1];
+  }
+
+  execute = (line) => {
+    if (this.PATTERNS.SET_GRID_MAX_POINT.test(line)) {
+      this.grid = new Grid();
+      return this.grid.setMaxPoint(
+        this.parseGridMaxPoint(line)
+      );
+    }
+
+    if (this.PATTERNS.SET_ROBOT_POSITION_AND_ORIENTATION.test(line)) {
+      return this.robots.push(
+        new Robot(this.parseRobotPositionAndOrientation(line))
+      );
+    }
+
+    if (this.PATTERNS.INSTRUCTIONS.test(line)) {
+      return this.getLastAddedRobot().execute(
+        this.parseInstructions(line), this.grid
+      );
+    }
+  }
+
+  parse = () => {
+    this.lineReader
+      .on('line', line => this.execute(line))
+      .on('close', () => this.results());
+  }
+
+  results() {
+    this.robots.map(robot =>
+      console.log(`${robot.position.x} ${robot.position.y} ${robot.orientation} ${robot.isLost ? 'LOST' : ''}`)
+    );
+  }
+}
+
+export default Interpreter;
